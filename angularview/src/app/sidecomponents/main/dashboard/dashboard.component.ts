e949b4f4fcca25d9c9af3faecff1d3b4e01ae40e
unknown <raj.bokade@crossasyst.com>
2022-12-13 18:15:59 +0530

    13 dec

--- angularview/src/app/sidecomponents/main/dashboard/dashboard.component.ts
+++ angularview/src/app/sidecomponents/main/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'node_modules/chart.js'
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-dashboard',
@@ -7,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
   type: any;
+  public chart: any;
   // dashboards=[{'viewValue':'listing'
 
   // },{'viewValue':'Real Estate'},{'viewValue':'Crypto'},{'viewValue':'CRM'}]
@@ -14,7 +17,39 @@ export class DashboardComponent implements OnInit {
 
   constructor() { }
 
+
   ngOnInit(): void {
+    this.createChart();
+  }
+
+  createChart() {
+
+    this.chart = new Chart("MyChart", {
+      type: 'line', //this denotes tha type of chart
+
+      data: {// values on X-Axis
+        labels: ['2022-05-10', '2022-05-11', '2022-05-12', '2022-05-13',
+          '2022-05-14', '2022-05-15', '2022-05-16', '2022-05-17',],
+        datasets: [
+          {
+            label: "Sales",
+            data: ['467', '576', '572', '79', '92',
+              '574', '573', '576'],
+            backgroundColor: 'blue'
+          },
+          {
+            label: "Profit",
+            data: ['542', '542', '536', '327', '17',
+              '0.00', '538', '541'],
+            backgroundColor: 'limegreen'
+          }
+        ]
+      },
+      options: {
+        aspectRatio: 2.5
+      }
+
+    });
   }
 
   cards = [{ 'subtitle': '25363', 'title': 'Listed Property', 'percentage': ' 03%', 'small': ' This Week', 'value': '30.45' },
